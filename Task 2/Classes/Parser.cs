92559d26d1ca90e21f6e02b28d8fa3b68abe6ce5
CarryBerry <31474124+CarryBerry@users.noreply.github.com>
2017-11-13 16:45:46 +0300

    Added additional classes, rebuilded already existed. Now all main objects are string.

---
+++ Task 2/Classes/Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Task_2.Interfaces;
+
+namespace Task_2.Classes
+{
+    public class Parser
+    {
+        public Text Parse(IEnumerable<string> sentences)
+        {
+            {
+                var text = new Text(new List<ISentence>(), new Sentence());
+                const string pattern = @"(\w+)|(\p{P})";
+
+                foreach (var currentSentence in sentences)
+                {
+                    var sentence = new Sentence(new List<ISentenceItem>(), new Word());
+                    var matches = Regex.Matches(currentSentence, pattern);
+
+                    foreach (Match match in matches)
+                    {
+                        sentence.AddElementToEnd(Regex.IsMatch(match.Value, @"(\p{P})")
+                        ? new SentenceItem(match.Value, SentenceItemType.PunctuationSign)
+                        : new SentenceItem(match.Value, SentenceItemType.Word));
+                    }
+                    text.AddSentence(sentence);
+                }
+                return text;
+            }
+        }
+    }
+}
